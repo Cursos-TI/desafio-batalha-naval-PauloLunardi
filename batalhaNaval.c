1e029815b83dfade1f698f87625aa9a03d1e28b2
Paulo Lunardi <lunardi.hb@gmail.com>
2025-12-05 00:33:49 +0000

    Desafio: Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,9 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-// Nível Novato - Posicionamento dos Navios
-
-#include <stdio.h>
+// Desafio: Nível Aventureiro - Posicionamento dos Navios em diagonal
 
 int main(){
 
@@ -21,8 +19,8 @@ int main(){
             }
         }
 
-    // Declara e inicializa os vetores do primeiro navio
-    int navio_1_linha[3] = {2, 2, 2};
+    // Declara e inicializa os vetores do primeiro navio na horizontal
+    int navio_1_linha[3] = {0, 0, 0};
     int navio_1_coluna[3] = {2, 3, 4};
 
     for(int i = 0; i < 3; i++){
@@ -30,18 +28,36 @@ int main(){
         tabuleiro_batalha[navio_1_linha[i]][navio_1_coluna[i]] = 3;
     }
 
-    // Declara e inicializa os vetores do segundo navio
-    int navio_2_linha[3] = {5, 6, 7};
-    int navio_2_coluna[3] = {7, 7, 7};
+    // Declara e inicializa os vetores do segundo navio na vertical
+    int navio_2_linha[3] = {3, 4, 5};
+    int navio_2_coluna[3] = {8, 8, 8};
 
     for(int i = 0; i < 3; i++){
 
         tabuleiro_batalha[navio_2_linha[i]][navio_2_coluna[i]] = 3;
     }
 
+    // Declara e inicializa os vetores do terceiro navio na diagonal
+    for(int i = 2; i < 5; i++){
+        for(int j = 2; j < 5; j++){
+            if(i == j){
+            tabuleiro_batalha[i][j] = 3;
+            }
+        }
+    }
+
+    // Declara e inicializa os vetores do quarto navio na diagonal
+    for(int i = 8; i > 5; i--){
+        for(int j = 1; j < 5; j++){
+            if(i + j == 9){
+            tabuleiro_batalha[i][j] = 3;
+            }
+        }
+    }
+
     // IMprime o indice das colunas
     printf("*** TABULEIRO BATALHA NAVAL ***\n");
-    printf("  ");
+    printf("   ");
     for( int j = 0; j < 10; j++){
         printf("%d ", coluna[j]);
     }
@@ -50,7 +66,7 @@ int main(){
     
     for(int i = 0; i < 10; i++){
         //  Imprime o indice da linhas
-        printf("%d ", linha[i]); 
+        printf("%2d ", linha[i]); 
 
         for( int j = 0; j < 10; j++){
             printf("%d ",tabuleiro_batalha[i][j]);
@@ -61,6 +77,3 @@ int main(){
 
     return 0;
 }
-
-
-    
